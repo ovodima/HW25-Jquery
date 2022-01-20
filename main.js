d19ff7497b652aa079b2811e95fbe505b57d24b8
OvoDima <dima.tsymbal12345@gmail.com>
2022-01-20 13:57:27 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// $(".test").css("color", "red");
+// $("h2, .test").css("color", "red");
+// $("p + p").css("color", "red");
+// $("li.www ~ li").css("color", "red");
+// $("h2 > i").css("color", "red");
+
+
