e96ab1f28e4c501ba4b07da3bda3463f3f0744b6
Dima <dima.tsymbal12345@gmail.com>
2022-01-21 19:42:49 +0200

    hw compl

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
-// $(".test").css("color", "red");
-// $("h2, .test").css("color", "red");
-// $("p + p").css("color", "red");
-// $("li.www ~ li").css("color", "red");
-// $("h2 > i").css("color", "red");
+$(".test").css("color", "red");
+$("h2, .test").css("color", "red");
+$("p + p").css("color", "red");
+$("li.www ~ li").css("color", "red");
+$("h2  i").css("color", "red");
+$("h2 > i").css("color", "orange");
 
+ $("h2:not(.test)").css("color", "red");
+ $(":header:not(.test)").css("color", "green");
+ $(":header").next(":header").css("color", "yellow");
+ $(":header.test").next(":header.test").css("color", "blue");
 
